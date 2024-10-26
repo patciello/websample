ea3775f8e79cb94cdc83e9baccb578b9fd3ac7e3
Leonardo Paciello Ferreira Dias <leonardopaciellop@gmail.com>
2024-10-26 18:08:32 -0300

    Upgrade - Home,Contact and DogLists

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,15 @@
+/* src/components/Contact.js */
 import React from "react";
 
 function Contact() {
   return (
-    <div>
+    <div className="container">
       <h1>Contato</h1>
       <form>
         <label>Nome:</label>
         <input type="text" name="name" />
-        <br />
         <label>Mensagem:</label>
         <textarea name="message"></textarea>
-        <br />
         <button type="submit">Enviar</button>
       </form>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
+/* src/components/Home.js */
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Bem-vindo ao Canil</h1>
-      <p>Encontre o seu novo amigo aqui!</p>
+      <p>
+        Encontre o seu novo amigo aqui! Navegue pela lista de cães disponíveis
+        para adoção e descubra qual deles é perfeito para sua família.
+      </p>
     </div>
   );
 }
