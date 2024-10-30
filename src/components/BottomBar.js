9f229a898300e9b3149585f7f1d179f9361f9a05
Leonardo Paciello Ferreira Dias <leonardopaciellop@gmail.com>
2024-10-29 22:42:33 -0300

    Update - Bottom Styling

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -3,6 +3,12 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { FaHome, FaSearch, FaPlus, FaBell, FaUser } from "react-icons/fa";
 
+//Icons Set
+import { GoHome } from "react-icons/go";
+import { TbCategory } from "react-icons/tb";
+import { MdOutlineEventAvailable } from "react-icons/md";
+import { PiHandsPrayingDuotone } from "react-icons/pi";
+
 const BottomBarContainer = styled.div`
   position: fixed;
   bottom: 0;
@@ -10,18 +16,23 @@ const BottomBarContainer = styled.div`
   right: 0;
   height: 80px;
   padding-bottom: 20px; /* Ajuste esse valor para garantir uma distância confortável do Home Indicator */
-  background: linear-gradient(to top, #000000 20%, #282c34 80%);
+  background: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.8),
+    rgba(40, 44, 52, 0.8)
+  );
   display: flex;
   justify-content: space-around;
   align-items: center;
-  z-index: 10; /* Mantém a barra acima de outros elementos */
+  z-index: 10;
+  backdrop-filter: blur(10px);
 `;
 const BottomBarLowerSection = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
-  height: 20px; /* Altura da parte preta na parte inferior */
+  height: 10px; /* Altura da parte preta na parte inferior */
   background-color: #000000; /* Cor preta para a parte inferior */
 `;
 
@@ -38,7 +49,7 @@ const IconButton = styled(motion.button)`
   cursor: pointer;
 
   &:hover {
-    color: #61dafb;
+    color: #2e2e2e;
   }
 
   span {
@@ -50,25 +61,17 @@ const BottomBar = () => {
   return (
     <>
       <BottomBarContainer>
-        <IconButton whileHover={{ scale: 1.2, color: "#61dafb" }}>
-          <FaHome />
-          <span>Home</span>
+        <IconButton whileHover={{ scale: 1.2, color: "#bfbfbf" }}>
+          <GoHome />
         </IconButton>
-        <IconButton whileHover={{ scale: 1.2, color: "#61dafb" }}>
-          <FaSearch />
-          <span>Search</span>
+        <IconButton whileHover={{ scale: 1.2, color: "#bfbfbf" }}>
+          <TbCategory />
         </IconButton>
-        <IconButton whileHover={{ scale: 1.2, color: "#61dafb" }}>
-          <FaPlus />
-          <span>Scan</span>
+        <IconButton whileHover={{ scale: 1.2, color: "#bfbfbf" }}>
+          <MdOutlineEventAvailable />
         </IconButton>
-        <IconButton whileHover={{ scale: 1.2, color: "#61dafb" }}>
-          <FaBell />
-          <span>History</span>
-        </IconButton>
-        <IconButton whileHover={{ scale: 1.2, color: "#61dafb" }}>
-          <FaUser />
-          <span>Profile</span>
+        <IconButton whileHover={{ scale: 1.2, color: "#bfbfbf" }}>
+          <PiHandsPrayingDuotone />
         </IconButton>
       </BottomBarContainer>
       <BottomBarLowerSection />
